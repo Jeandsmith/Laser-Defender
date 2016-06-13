7c6f79a9ba927fe57a3e5a5512c9d66e40f1b258
James Smith <jeandsmith27@gmail.com>
2016-06-12 19:10:16 -0700

    Bug fixes and additional bugs within the code. There is a problem spawning the player after the game object has been destroy. I think is due to not being able to access the script after the player object has been destroy.

---
+++ Assets/AllScripts/Entities/Player/PlayerCollisionTrigger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine.Experimental.Director;
+
+public class PlayerCollisionTrigger: Ship
+{
+	public override void Start ()
+	{
+		MyHealth = 5;
+	}
+
+	//Check collision trigger
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+		EnemyBullet eBullet = other.gameObject.GetComponent<EnemyBullet> ();
+
+		//Check that the player bullet is not null
+		if (eBullet != null)
+		{
+			MyHealth -= eBullet.Damage ();
+
+			//Check if the player's health is at 0
+			if (MyHealth <= 0)
+			{
+				MyHealth = 0;
+
+				//Trying to spawn the player afte the player have been destroy.
+				//There is a problems spawning the player after destroy. My hypothesis is that i cant access the script after
+				//The ship have been destroy.
+				LifeController manageLifes = FindObjectOfType<LifeController> ();
+				manageLifes.ReduceLive ();
+				if (manageLifes.ShipLifeCount != 0)
+				{
+					print ("Re-spawning Player");
+					LifeController.PlayerRespawn pRespawn = GameObject.Find ("LifeController").GetComponent
+						<LifeController.PlayerRespawn> ();
+					pRespawn.RespawnPlayer ();
+					Destroy (gameObject);
+				}
+				
+				//This piece of code is working properly. 
+				else if (manageLifes.ShipLifeCount == 0)
+				{
+					//Load the leader board scene.
+					LevelManager levManager = GameObject.Find ("LevelManager").GetComponent<LevelManager> ();
+					levManager.LoadLevel ("Leader-board");
+				}
+				SpawnExplosion (2f);
+			}
+			eBullet.Hit ();
+		}
+	}
+}
---
+++ Assets/AllScripts/Entities/Player/SpawnPoisition.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoisition: MonoBehaviour
+{
+	//private Player _playerShip = null;
+
+	//private void Update()
+	//{
+	//	CheckForPlayer();	
+	//}
+
+	////Check if the player is in the scene
+	//private void CheckForPlayer()
+	//{
+	//	if (_playerShip == null)
+	//	{
+	//		Instantiate(_playerShip, transform.position, Quaternion.identity);
+	//	}else if (_playerShip.GetInstanceID() != _playerShip.gameObject.GetInstanceID())
+	//	{
+	//		Destroy(_playerShip.gameObject);
+	//	}
+	//}
+
+	//Draw gizmo
+	private void OnDrawGizmos ()
+	{
+		Gizmos.DrawWireSphere(transform.position, 1f);
+	}
+}
